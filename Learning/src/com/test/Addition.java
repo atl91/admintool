3121fa5c4d8360ea92efce5e5e9e8e2018c87aad
Atul <Atul@DESKTOP-JAM9RVN>
2020-05-28 16:30:19 +0530

    first commit

---
+++ Learning/src/com/test/Addition.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =56;
+		
+		System.out.println("outpu" + a);
+
+	}
+
+}
